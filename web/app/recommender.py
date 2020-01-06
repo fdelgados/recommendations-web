c334eeaf374bae3ebaba3e048f3439211f52e6eb
Cisco Delgado <fdelgados@gmail.com>
2020-01-06 19:56:26 +0100

    Add docstring to recommender module

--- web/app/recommender.py
+++ web/app/recommender.py
@@ -1,79 +1,102 @@
-import pandas as pd
 import numpy as np
-from typing import List
-from .models import LeadRepository, Lead
+from . import db
+from sqlalchemy import text
+from .models import CourseRepository
+
+
+def find_neighbours(user_id: str) -> np.ndarray:
+    """Find similar users in database
+
+    :param user_id: Identifier of the user for whom we want to search neighbours
+    :return: An array of similar user identifiers
+    """
+    query = '''SELECT another_user_id
+                FROM users_similarities 
+                WHERE a_user_id = :user_id
+                ORDER BY similarity DESC'''
+
+    result = db.engine.execute(text(query), user_id=user_id)
+
+    return np.array([row['another_user_id'] for row in result])
 
 
 class Recommender:
-    def __init__(self, lead_repository: LeadRepository, user_item_matrix_file: str):
-        self.lead_repository = lead_repository
-        self.user_item_matrix_file = user_item_matrix_file
+    """Makes courses recommendations"""
 
-    def read_leads_user_item_matrix(self, chunk_size: int = 500):
-        return pd.read_csv(self.user_item_matrix_file, chunksize=chunk_size).set_index('user_id')
+    def __init__(self):
+        """Recommender constructor. Initializes the object."""
 
-    def find_similar_users(self, user_id: str, user_item_matrix: pd.DataFrame) -> np.ndarray:
+        self.by_leads = {}
+        self.by_content = {}
+        self.by_rating = {}
+        self.by_number_of_leads = {}
+        self.by_user = {}
+        self.course_repository = CourseRepository()
+
+    def make_recommendations_by_course(self, course_id, max_recommendations: int = 10) -> 'Recommender':
+        """Make user interaction and content based recommendations
+
+        :param course_id: Identifier of the course for which we want to find similar
+        :param max_recommendations: Maximum number of recommendations
+        :return: `Recommender` class
         """
-        Creates an array of similar users based on leads generated on the same courses
+        self.by_leads = self.course_repository.find_similar_by_leads(course_id, max_recommendations)
+        self.by_content = self.course_repository.find_similar_by_content(course_id, max_recommendations)
 
-        :param user_id: User id for which we want to find similar users
-        :param user_item_matrix: Leads user-item matrix
+        return self
 
-        :return: Array of similar users sorted by similarity
+    def make_rank_recommendations(self, category_id: int = None, exclude_course_id: str = None,
+                                  max_recommendations: int = 10) -> 'Recommender':
+        """Make rank based recommendations
+
+        :param category_id: Category identifier if we want to make recommendations of this category
+        :param exclude_course_id: Course identifier if we want to exclude it from the recommendations
+        :param max_recommendations: Maximum number of recommendations
+        :return: `Recommender` class
         """
+        self.by_rating = self.course_repository.find_sorted_by_rating(category=category_id,
+                                                                      max_rows=max_recommendations,
+                                                                      exclude=exclude_course_id)
+        self.by_number_of_leads = self.course_repository.find_sorted_by_leads(category=category_id,
+                                                                              max_rows=max_recommendations,
+                                                                              exclude=exclude_course_id)
 
-        user = np.array(user_item_matrix.loc[user_id])
+        return self
 
-        similarities = dict()
+    def make_recommendations_by_user(self, user_id: str = None, max_recommendations: int = 10) -> 'Recommender':
+        """Makes neighbourhood based recommendations
 
-        for another_user_id, courses in user_item_matrix.iterrows():
-            if user_id == another_user_id:
-                continue
-
-            similarities[another_user_id] = np.dot(user, np.array(courses))
-
-        sorted_similarities = sorted(similarities.items(), key=lambda item: item[1], reverse=True)
-
-        return np.array([id for (id, similarity) in sorted_similarities])
-
-    def get_user_leads(self, user_id: str) -> List[Lead]:
+        :param user_id: User identifier to which we want to recommend courses
+        :param max_recommendations: Maximum number of recommendations
+        :return: `Recommender` class
         """
-        Returns a list of courses to which the user has generated lead
+        if not user_id:
+            return self
 
-        :param user_id: User id for which we want to find generated leads
+        user_courses = self.course_repository.find_by_user_leads(user_id)
+        user_courses_ids = np.array(list(user_courses.keys()))
 
-        :return list: List of courses to which the user has generated lead
-        """
+        if len(user_courses_ids) == 0:
+            return self
 
-        return self.lead_repository.find_by_user(user_id)
+        rec_courses_ids = np.array([])
+        neighbours_courses = {}
 
-    def leads_based_recommendations(self, course_id: str, max_recs: int = 10):
-        """
-        Returns an array of recommended courses for a user based on generated leads
+        similar_users = find_neighbours(user_id)
+        for similar_user_id in similar_users:
+            neighbour_courses = self.course_repository.find_by_user_leads(similar_user_id)
+            neighbours_courses.update(neighbour_courses)
 
-        :param course_id: User id for which we want to make the recommendations
-        :param max_recs: Maximum number of recommendations
+            new_recs = np.setdiff1d(np.array(list(neighbour_courses.keys())), user_courses_ids, assume_unique=True)
+            rec_courses_ids = np.unique(np.concatenate([new_recs, rec_courses_ids], axis=0))
 
-        :return numpy.array: Array of courses recommended based on generated leads
-        """
-        similar_users = np.array([])
-
-        leads_user_item_matrix = self.read_leads_user_item_matrix()
-
-        for df in leads_user_item_matrix:
-            similar_users = np.concatenate([similar_users, df[df.loc[:, course_id] == 1].index], axis=0)
-
-        user_courses = np.array(course_id)
-
-        recs = np.array([])
-
-        for user in similar_users:
-            neighbs_leads = self.get_user_leads(user)
-
-            new_recs = np.setdiff1d(neighbs_leads, user_courses, assume_unique=True)
-            recs = np.unique(np.concatenate([new_recs, recs], axis=0))
-
-            if len(recs) > max_recs:
+            if len(rec_courses_ids) > max_recommendations:
                 break
 
-        return recs[:max_recs]
+        if len(rec_courses_ids) == 0:
+            return self
+
+        self.by_user = {course_id: course for (course_id, course) in neighbours_courses.items()
+                        if course_id in rec_courses_ids}
+
+        return self
